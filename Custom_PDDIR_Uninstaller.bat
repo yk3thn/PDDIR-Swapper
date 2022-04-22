a50b23d1f4efff0a2d6a63296f66d243ceee1308
PeenkLion <65327271+PeenkLion@users.noreply.github.com>
2022-04-21 21:21:12 -0400

    Add files via upload

---
+++ Custom_PDDIR_Uninstaller.bat
@@ -0,0 +1,4 @@
+@echo made by peenklion
+IF EXIST "%userprofile%\AppData\Local\FortniteGame\Saved\PersistentDownloadDir\CMS\Files\C28FF1DE0C661DAF01E118A30B3F21B897A7A6E2" DEL /F "%userprofile%\AppData\Local\FortniteGame\Saved\PersistentDownloadDir\CMS\Files\C28FF1DE0C661DAF01E118A30B3F21B897A7A6E2"
+@echo Done!
+@pause
